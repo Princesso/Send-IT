f23b79419e2c3ffcbc8555914bbb33f304f05913
princesso <egbunaoluebube@gmail.com>
2018-11-03 09:04:06 +0100

    update server root, create a route for the parcels api, separation of concerns

--- server/server/app.js
+++ server/server/app.js
@@ -2,6 +2,20 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import path from 'path'
 
-const app = express();
+let app = express();
 
 import parcelRoute from './routes/parcels'
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/api/v1/parcels', parcelRoute)
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ msg: 'Welcome to Send-It API'});
+});
+
+app.listen(8080, () => console.log('app Running on port 8080'));
+
+export default app;
