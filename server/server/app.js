3681ee9b5330f754ba2f25abde416f8a916afc54
princesso <egbunaoluebube@gmail.com>
2018-11-05 15:11:32 +0100

    update server root file

--- server/server/app.js
+++ server/server/app.js
@@ -5,12 +5,14 @@ import path from 'path'
 let app = express();
 
 import parcelRoute from './routes/parcels'
+import userRoute from './routes/users'
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/api/v1/parcels', parcelRoute)
+app.use('/api/v1/users',userRoute)
 
 app.get('/', (req, res) => {
   return res.status(200).json({ msg: 'Welcome to Send-It API'});
