022994ef8a536ae2abd21a25a42ba6adb00528a0
princesso <egbunaoluebube@gmail.com>
2018-11-12 06:00:16 +0100

    create users table

--- server/models/database.js
+++ server/models/database.js
@@ -14,3 +14,25 @@ pool.on('connect', () => {
   console.log('database connection established');
 });
 
+const createTables = async () => {
+  const userTable = `
+    CREATE TABLE IF NOT EXISTS
+      users(
+        userid SERIAL PRIMARY KEY,
+        email VARCHAR(128) UNIQUE,
+        phone VARCHAR(128) NOT NULL,
+        password VARCHAR(128) NOT NULL
+      )
+  `;
+  await pool.query(userTable)
+    .then((res) => {
+      console.log('users table created!: ', res);
+      // pool.end();
+    })
+    .catch((err) => {
+      console.log('An error occured while creating users table: ', err);
+      pool.end();
+    });
+};
+
+createTables().then(res => console.log('All tables created'))
