7269bff444ed946894dda00e66f63cde951c9dec
princesso <egbunaoluebube@gmail.com>
2018-11-14 09:34:03 +0100

    separate parcels  mockdata

---
+++ server/mockdata/parcels.js
@@ -0,0 +1,25 @@
+
+let parcels = [
+  {
+    id: 1, 
+    ownerid: 4563, 
+    item_name: 'Shoes', 
+    weigth: 4, 
+    description: "Non edible", 
+    from: 'Yaba', 
+    status: "pending", 
+    destination: 'Ojuelegba'
+  },
+  {
+    id: 2, 
+    ownerid: 4573, 
+    item_name: 'Chocolates', 
+    weigth: 2, 
+    description: "edible", 
+    from: 'Abuja', 
+    status: "delivered", 
+    destination: 'Enugu'
+  }
+]
+
+export default parcels
