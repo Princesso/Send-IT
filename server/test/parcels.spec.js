7e2d54edb8f378ec53ebb659e7f931a609685b24
princesso <egbunaoluebube@gmail.com>
2018-11-18 18:19:59 +0100

    update unit tests

--- server/test/parcels.spec.js
+++ server/test/parcels.spec.js
@@ -20,8 +20,6 @@ describe('Parcel delivery orders', () => {
       request
         .get('/api/v1/parcels')
         .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.a('array');
               res.should.be.json;
           done();
         });
@@ -31,22 +29,30 @@ describe('Parcel delivery orders', () => {
   describe('/POST parcel delivery orders', () => {
     it('should post a parcel delivery order', (done) => {
       let newOrder = {
-        item_name: 'Bag',
-        weight: 6,
-        from: 'Yaba',
-        destination: 'Enugu'
-      }
+        placedBy: req.body.placedBy,
+        weight: req.body.weight,
+        weightmetric: req.body.weightmetric,
+        sentOn: req.body.sentOn,
+        deliveredOn: req.body.deliveredOn,
+        status: req.body.status,
+        fromAddress: req.body.fromAddress,
+        toAddress: req.body.toAddress,
+        currentLocation: req.body.currentLocation
+      };
       chai.request(app)
           .post('/api/v1/parcels')
           .send(newOrder)
           .end((err, res) => {
             res.should.have.status(200);
             res.should.be.json;
-            res.body.should.have.property('id');
-            res.body.should.have.property('item_name');
-            //res.body.should.have.property('weight');
-            res.body.should.have.property('from');
-            res.body.should.have.property('destination');
+            res.body.should.have.property('placedBy');
+            res.body.should.have.property('weight');
+            res.body.should.have.property('weightmetric');
+            res.body.should.have.property('sentOn');
+            res.body.should.have.property('status');
+            res.body.should.have.property('fromAddress');
+            res.body.should.have.property('toAddress');
+            res.body.should.have.property('currentLocation');
             done();
           });
     });
