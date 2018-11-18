20ba5057ff741299c33b1a82494a47aeb7ee1fa0
princesso <egbunaoluebube@gmail.com>
2018-11-18 07:40:02 +0100

    add authentication middleware

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import path from 'path'
 import cors from 'cors'
+import jwt from 'jsonwebtoken'
 
 let app = express();
 
 import parcelRoute from './routes/parcels'
 import userRoute from './routes/users'
+import authRoute from './routes/auth'
 
 app.set('views', path.join(__dirname, 'views'));
 
@@ -28,12 +30,39 @@ app.all('/*', function(req, res, next) {
   next();
 });
 
+app.use('/api/v1/auth', authRoute)
+
+//middleware to ensure routes are authenticated before they are acessed. Always put a middleware only above routes you want to work with so that it isn't an overkill
+app.use( (req, res, next) => {
+  const bearerHeader = req.headers['authorization']
+  if(typeof bearerHeader !== 'undefined') {
+    const bearerArray = bearerHeader.split(" ")
+    const bearerToken = bearerArray[1]
+    req.token = bearerToken
+
+    jwt.verify(req.token, process.env.SECRET, (err, data) => {
+      if (err) {
+        res.send("You cannot access this page")
+      } else {
+        console.log(" I got to the middleware ")
+      }
+    })
+  } else {
+    res.json("Forbidden")
+  }
+  next()
+})
+
 app.use('/api/v1/parcels', parcelRoute)
 app.use('/api/v1/users', userRoute)
 
 app.get('/', (req, res) => {
   return res.status(200).json({ msg: 'Welcome to Send-It API'});
 });
+
+app.use(function (err, req, res, next) {
+  res.status(500).send('Something broke!')
+})
 
 const port = process.env.PORT || 8080;
 
