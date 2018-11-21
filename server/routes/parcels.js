621ff126816043a0f1fbabc7fd3c900b1bc81776
princesso <egbunaoluebube@gmail.com>
2018-11-21 02:32:52 +0100

    remove unnecessary imports and use more meaningful naming

--- server/routes/parcels.js
+++ server/routes/parcels.js
@@ -2,21 +2,21 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import parcels from '../mockdata/parcels'
 import Joi from 'joi'
-import { pool, dbQuery } from '../database'
+import { pool, dbQuery } from '../config'
 import moment from 'moment'
 import nodeMailer from 'nodemailer'
-import parcelsFromDatabase from '../controllers/parcels'
+import parcelController from '../controllers/parcels'
 import Auth from '../middleware/auth'
 
 let router = express.Router();
 router.use(express.json())
 
-router.get('/',Auth.verifyToken, parcelsFromDatabase.getAll)
-router.get('/:id',Auth.verifyToken, parcelsFromDatabase.getOne)
-router.post('/',Auth.verifyToken, parcelsFromDatabase.create)
-router.patch('/:id/cancel',Auth.verifyToken, parcelsFromDatabase.cancel)
-router.patch('/:id/destination',Auth.verifyToken, parcelsFromDatabase.changeDestination)
+router.get('/',Auth.verifyToken, parcelController.getAll)
+router.get('/:id',Auth.verifyToken, parcelController.getOne)
+router.post('/',Auth.verifyToken, parcelController.create)
+router.patch('/:id/cancel',Auth.verifyToken, parcelController.cancel)
+router.patch('/:id/destination',Auth.verifyToken, parcelController.changeDestination)
 
-router.patch('/:id/currentlocation',Auth.verifyToken, parcelsFromDatabase.changeCurrentLocation)
+router.patch('/:id/currentlocation',Auth.verifyToken, parcelController.changeCurrentLocation)
 
 export default router;
