a70277876f7f80acc540b188465d934841635dde
princesso <egbunaoluebube@gmail.com>
2018-11-21 02:50:37 +0100

    move SQL queries to controller

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,44 +1,12 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-import Joi from 'joi'
-import users from '../mockdata/users'
-import { pool, dbQuery } from '../database'
-import dotenv from 'dotenv'
+import Auth from '../middleware/auth'
+import UsersFromDatabase from '../controllers/users'
 
 let router = express.Router();
 router.use(express.json())
 
-router.get('/', (req, res) => {
-  dbQuery('SELECT * FROM users', [], (err, dbres) => {
-    if(err) {
-      res.send({ "status": res.statusCode, "error": 'An error occured while trying to retrieve users'})
-    } else {
-      res.send({"status": res.statusCode, "data": dbres.rows[0]});
-    }
-  })
-});
-
-router.get('/:id', (req, res) => {
-  let id = req.params.id
-  dbQuery('SELECT * FROM users WHERE ID=$1',[id], (err, dbres) => {
-    if (err) {
-      res.send({ "status": res.statusCode, "error": 'No such User please check the ID again'})
-    } else {
-      res.send({"status": res.statusCode, "data": dbres.rows[0]});
-    }
-  })
-});
-
-router.get('/:id/parcels', (req,res) => {
-  const id = req.params.id
-  dbQuery('SELECT * FROM parcels WHERE placedBy=$1',[id], (err, dbres) => {
-    if (err) {
-      res.send({ "status": res.statusCode, "error": 'An error occured while trying to retrieve user parcels'})
-    } else {
-      res.send({"status": res.statusCode, "data": dbres.rows});
-    }
-  })
-});
-
+router.get('/', UsersFromDatabase.getAll);
+router.get('/:id',Auth.verifyToken,UsersFromDatabase.getOne);
+router.get('/:id/parcels',Auth.verifyToken,UsersFromDatabase.getUserParcels )
 
 export default router;
