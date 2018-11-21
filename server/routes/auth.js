dc45b362708889ef87a35fed70d92026975ed272
princesso <egbunaoluebube@gmail.com>
2018-11-21 02:32:01 +0100

    remove unnecessary imports

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,21 +1,13 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-import Joi from 'joi'
-import users from '../mockdata/users'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import moment from 'moment'
-import { pool, dbQuery } from '../database'
 import dotenv from 'dotenv'
-import UsersFromDatabase from '../controllers/users'
+import userController from '../controllers/users'
 
 dotenv.config();
 
 let router = express.Router();
 router.use(express.json())
 
-router.post('/signup', UsersFromDatabase.create)
-
-router.post('/login', UsersFromDatabase.login)
+router.post('/signup', userController.signup)
+router.post('/login', userController.login)
 
 export default router
