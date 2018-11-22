99c801adf3b97d1ca0d26eb1a70a468f8cb64cdb
princesso <egbunaoluebube@gmail.com>
2018-11-22 04:13:16 +0100

    move helper file into new directory helper

---
+++ server/helpers/helper.js
@@ -0,0 +1,30 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+import nodeMailer from 'nodemailer'
+
+dotenv.config();
+
+const Helper = {
+  hashPassword(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+  },
+  comparePassword(hashPassword, password) {
+    return bcrypt.compareSync(password, hashPassword);
+  },
+  isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  },
+  generateToken(payload) {
+    const token = jwt.sign(
+      payload,
+      process.env.SECRET, { expiresIn: '20d' }
+    );
+    return token;
+  },
+  sendEmail() {
+   
+  }
+}
+
+export default Helper;
