cfa69884d8dab97e765bf7ecca56d8ecbf41b9cf
princesso <egbunaoluebube@gmail.com>
2018-11-22 04:13:43 +0100

    update tests

---
+++ server/test/auth.spec.js
@@ -0,0 +1,54 @@
+import Auth from '../controllers/UsersController'
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+import app from '../app'
+
+let should = chai.should();
+chai.use(chaiHttp)
+const request = chai.request(app)
+
+describe ('Authentication Tests', () => {
+  afterEach((done) => {
+    done();
+    console.log("In test")
+  });
+  describe('/POST login to the application', () => {
+    it('return a token and a message', (done) => {
+      let loginData = {
+       email: "egbunaoluebube@gmail.com",
+       password: "princess"
+      };
+      chai.request(app)
+          .post('/api/v1/auth/login')
+          .send(loginData)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.have.property('token');
+            res.body.should.have.property('message');
+            done();
+          });
+    });
+  });
+  describe('/POST signup already existing user', () => {
+    it('should not be successful', (done) => {
+      let loginData = {
+        firstname: "Udum",
+        lastname: "Ngozi",
+        email: "udumNgozi@gmail.com",
+        username: "udungozi.123",
+        password: "princess"
+      };
+      chai.request(app)
+          .post('/api/v1/auth/signup')
+          .send(loginData)
+          .end((err, res) => {
+            res.should.have.status(500);
+            res.should.be.json;
+            res.body.should.have.property('status');
+            res.body.should.have.property('message');
+            done();
+          });
+    });
+  });
+})
