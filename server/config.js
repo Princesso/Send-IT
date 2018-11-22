3e5b64e8e29298f349fba365c36753406051b883
princesso <egbunaoluebube@gmail.com>
2018-11-22 08:12:49 +0100

    Create tables Asynchronously if they don't exist

--- server/config.js
+++ server/config.js
@@ -9,4 +9,60 @@ const client = new Client({
 
 client.connect()
 
+const createTables = async () => {
+  const userTable = `
+    CREATE TABLE IF NOT EXISTS
+      users(
+        id SERIAL PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        othernames VARCHAR(30),
+        email VARCHAR(128) UNIQUE,
+        username VARCHAR(128) NOT NULL,
+        registered TIMESTAMP,
+        isAdmin BOOLEAN,
+        password VARCHAR NOT NULL
+      )
+  `;
+  console.log("In config file before user")
+
+  await client.query(userTable)
+    .then((res) => {
+      console.log('users table created!: ', res);
+    })
+    .catch((err) => {
+      console.log('An error occured while creating users table: ', err);
+      client.end();
+    });
+  console.log("In config file 1")
+
+  const parcelsTable = `
+    CREATE TABLE IF NOT EXISTS
+      parcels(
+        id SERIAL PRIMARY KEY,
+        placedBy INTEGER REFERENCES users(id),
+        weight FLOAT NOT NULL, 
+        weightmetric VARCHAR NOT NULL,
+        sentOn TIMESTAMP,
+        deliveredOn DATE,
+        status VARCHAR NOT NULL,
+        fromAddress VARCHAR NOT NULL,
+        toAddress VARCHAR NOT NULL,
+        currentLocation VARCHAR
+      )
+  `;
+  console.log("In config file")
+  client.query(parcelsTable)
+  .then((res) => {
+    console.log('parcels table created!: ', res);
+  })
+  .catch((err) => {
+    console.log('An error occured while creating parcels table: ', err);
+    client.end();
+  });
+};
+
+
+createTables().then(res => console.log('All tables created'));
+
 export default  client
