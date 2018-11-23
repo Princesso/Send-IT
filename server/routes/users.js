a1deb42e8be9da5da582f4fa0e7697aa34a611f7
princesso <egbunaoluebube@gmail.com>
2018-11-23 02:39:32 +0100

    update users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,9 +5,9 @@ import UsersFromDatabase from '../controllers/UsersController'
 let router = express.Router();
 router.use(express.json())
 
-router.get('/', UsersFromDatabase.getAll);
+router.get('/', Auth.verifyToken,UsersFromDatabase.getAll);
 router.get('/:id',Auth.verifyToken,UsersFromDatabase.getOne);
 router.get('/:id/parcels',Auth.verifyToken,UsersFromDatabase.getUserParcels )
-router.get('/:id/makeadmin',Auth.verifyToken,UsersFromDatabase.makeAdmin)
+router.post('/:id/makeadmin',Auth.verifyToken,UsersFromDatabase.makeAdmin)
 
 export default router;
