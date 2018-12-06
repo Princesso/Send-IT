f40979f0ef75861d39a099da3098731eb57739c2
princesso <egbunaoluebube@gmail.com>
2018-12-06 14:24:28 +0100

    Update main.js

--- UI/js/main.js
+++ UI/js/main.js
@@ -8,57 +8,26 @@ const getUserToken = () => {
   }
 }
 
-function submitRegistrationForm(e, d) {
-  e.preventDefault()
-  const registrationInfo = {
-    email: document.getElementById('email').value,
-    password: document.getElementById("password").value,
-    username: document.getElementById("username").value,
-    firstname: document.getElementById("firstname").value,
-    lastname: document.getElementById("lastname").value,
-  }
-  fetch('https://sendit-it.herokuapp.com/api/v1/auth/signup', {
-    method: "POST",
-    body: JSON.stringify(registrationInfo),
-  })
-  .then(function(response) {
-      return response.json();
-    })
-  .then((res) => {
-    if(res.status==200) window.location.href = `${basePath}/login.html`  
-    else {
-      debugger
-      let newParagraph = document.createElement("p")
-      let textNode = document.createTextNode("An error occurred while trying to register you")
-      newParagraph.appendChild(textNode)
-
-      let div = document.getElementById("register")
-      div.insertBefore(newParagraph, div.childNodes[0])
-      newParagraph.style.color('red')
-    }
-  })
-  .catch(error => {
-    console.log(error)
-  })
-}
-
-
-function submitLoginForm(e, d) {
+async function submitLoginForm(e, d) {
   e.preventDefault()
   const loginInfo = {
     email: document.getElementById('email').value,
     password: document.getElementById("password").value
   }
-  fetch('https://sendit-it.herokuapp.com/api/v1/auth/login', {
+ await fetch('https://sendit-it.herokuapp.com/api/v1/auth/login', {
     method: "POST",
     body: JSON.stringify(loginInfo),
+    headers: {"Content-Type": "application/json"}
   })
   .then(function(response) {
       return response.json();
     })
   .then((res) => {
+    console.log(res)
+    debugger
     if(!res.token) throw('no token in response')
-    document.cookie = `user_token=${res.token}`
+    //document.cookie = `user_token=${res.token}`
+    window.localStorage.setItem('user_token', res.token)
     window.location.href = `${basePath}/dashboard.html` 
   })
   .catch(error => {
@@ -66,6 +35,47 @@ function submitLoginForm(e, d) {
   })
 }
 
+function getParcels() {
+  fetch('https://sendit-it.herokuapp.com/api/v1/users/1/parcels')
+  .then(function(response) {
+    return response.json();
+  })
+  .then((res) => {
+    // we had stored the attributes to be filled in the rows using the data-attributes of the thead
+    // so we grab the thead (for the data attributes)
+    // and grab the tbody so we can append our new rows to them
+    let tableHeadRef = document.getElementById('dashboard-table').getElementsByTagName('thead')[0]
+    let tableRef = document.getElementById('dashboard-table').getElementsByTagName('tbody')[0]
+    
+    // we sort the data by id so it lines up neatly in asc order :)
+    // then we iterate
+    res.data.sort((d1, d2) => d1.id - d2.id).forEach(order => {
+
+      // we create a new row on the body of the table
+      let newRow   = tableRef.insertRow(tableRef.rows.length);
+
+      // we want to create as many cells as there are on the header
+      // notice we are leaving out the last row? For our button later :)
+      for(var i = 0;i < tableHeadRef.rows[0].cells.length - 1;i++) {
+        let newCell = newRow.insertCell(i);
+        
+        // text to be inserted in the cell?
+        // the order is an object in the format {id: 1, currentlocation: 'lagos', toaddress: 'Aba', ...}
+        // for the first cell in the header 
+        let newText = document.createTextNode(order[tableHeadRef.rows[0].cells[i].dataset.name])
+        newCell.appendChild(newText);
+      }
+      
+      let btn = document.createElement('button');
+      btn.innerText = 'View'
+      let tag = document.createElement('a')
+      tag.appendChild(btn)
+      let newCell  = newRow.insertCell(i);
+      newCell.appendChild(tag);
+    })
+  })
+  .catch(error => console.error('Error:', error))
+}
 
 document.getElementById("submitBtn").addEventListener("click", submitLoginForm);
-document.getElementById("registerBtn").addEventListener("click", submitRegistrationForm);
+//document.getElementById("registerBtn").addEventListener("click", submitRegistrationForm);
