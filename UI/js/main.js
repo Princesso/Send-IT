86fb823e5c2f8d10e1eb5574e88add47a476204d
princesso <egbunaoluebube@gmail.com>
2018-12-06 15:12:51 +0100

    Update main.js

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,4 +1,4 @@
-const basePath = "../../resources/pages";
+
 
 const getUserToken = () => {
   let tokenStr = document.cookie.split(';')
@@ -8,30 +8,7 @@ const getUserToken = () => {
   }
 }
 
-async function submitLoginForm(e, d) {
-  e.preventDefault()
-  const loginInfo = {
-    email: document.getElementById('email').value,
-    password: document.getElementById("password").value
-  }
- await fetch('https://sendit-it.herokuapp.com/api/v1/auth/login', {
-    method: "POST",
-    body: JSON.stringify(loginInfo),
-    headers: {"Content-Type": "application/json"}
-  })
-  .then(function(response) {
-      return response.json();
-    })
-  .then((res) => {
-    if(!res.token) throw('no token in response')
-    //document.cookie = `user_token=${res.token}`
-    window.localStorage.setItem('user_token', res.token)
-    window.location.href = `${basePath}/dashboard.html` 
-  })
-  .catch(error => {
-    console.log(error)
-  })
-}
+
 
 function getParcels() {
   fetch('https://sendit-it.herokuapp.com/api/v1/users/1/parcels')
@@ -75,5 +52,5 @@ function getParcels() {
   .catch(error => console.error('Error:', error))
 }
 
-document.getElementById("submitBtn").addEventListener("click", submitLoginForm);
+
 //document.getElementById("registerBtn").addEventListener("click", submitRegistrationForm);
